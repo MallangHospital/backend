491369c98022d8f705dd4f1ab27c769a2ec56913
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2024-10-15 23:11:27 +0900

    cm2

---
+++ src/main/java/com/mallang/backend/post/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.mallang.backend.post.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+    @CreationTimestamp
+    @Column(updatable = false) //수정할 때는 관여 안하게
+    private LocalDateTime createdTime;
+
+    @UpdateTimestamp
+    @Column(insertable = false)//처음 작성할 때는 관여 안하게
+    private LocalDateTime updatedTime;
+}
---
+++ src/main/java/com/mallang/backend/post/entity/PostEntity.java
@@ -0,0 +1,56 @@
+package com.mallang.backend.post.entity;
+
+import com.mallang.backend.post.dto.PostDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+// DB의 테이블 역할을 하는 클래스
+@Entity
+@Getter
+@Setter
+@Table(name = "post_table") //특정 테이블 이름을 따로 설정. 필수X
+public class PostEntity extends BaseEntity {
+    @Id // pk컬럼 지정. 필수
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(length = 20, nullable = false) // 컬럼의 크기를 설정, not null
+    private String postWriter;
+
+    @Column // 크기 255, null 가능
+    private String postPass;
+
+    @Column
+    private String postTitle;
+
+    @Column(length = 500)
+    private String postContents;
+
+    @Column
+    private int postHits;
+
+
+
+    public static PostEntity toSaveEntity(PostDTO postDTO){
+        PostEntity postEntity = new PostEntity();
+        postEntity.setPostWriter(postDTO.getPostWriter());
+        postEntity.setPostPass(postDTO.getPostPass());
+        postEntity.setPostTitle(postDTO.getPostTitle());
+        postEntity.setPostContents(postDTO.getPostContents());
+        postEntity.setPostHits(0);
+
+        return postEntity;
+    }
+    public static PostEntity toUpdateEntity(PostDTO postDTO) {
+        PostEntity postEntity = new PostEntity();
+        postEntity.setId(postDTO.getId());
+        postEntity.setPostWriter(postDTO.getPostWriter());
+        postEntity.setPostPass(postDTO.getPostPass());
+        postEntity.setPostTitle(postDTO.getPostTitle());
+        postEntity.setPostContents(postDTO.getPostContents());
+        postEntity.setPostHits(postDTO.getPostHits());
+        return postEntity;
+    }
+}
+
