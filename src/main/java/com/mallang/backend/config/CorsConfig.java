52961a0411d78b60b8c37631231c55161809cb45
sunyoung <minnie2002@naver.com>
2024-11-27 22:21:17 +0900

    Fix binding to Heroku PORT

--- src/main/java/com/mallang/backend/config/CorsConfig.java
+++ src/main/java/com/mallang/backend/config/CorsConfig.java
@@ -9,10 +9,11 @@ import org.springframework.web.cors.CorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
-    /*@Bean
+    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 Origin
+        configuration.addAllowedOrigin("http://localhost:5500"); // 허용할 Origin
         configuration.addAllowedOrigin("http://172.29.83.170:5500");
         configuration.addAllowedOrigin("https://mallang-a85bb2ff492b.herokuapp.com");
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
@@ -22,19 +23,5 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 설정 적용
         return source;
-    }*/
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*"); // 허용할 Origin
-        configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
-        configuration.addAllowedHeader("*"); // 모든 헤더 허용
-        configuration.setAllowCredentials(true); // 쿠키 허용
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 설정 적용
-        return source;
     }
-}
 }
