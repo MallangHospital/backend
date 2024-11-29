f2cf93932bbdb1ac222b3ae9a9eae82287ceb60f
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2024-11-30 02:25:59 +0900

    공지사항 오류 수정

--- src/main/java/com/mallang/backend/dto/NoticeDTO.java
+++ src/main/java/com/mallang/backend/dto/NoticeDTO.java
@@ -8,10 +8,10 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NoticeDTO {
-    private String id; //공지사항 ID
+    private Long id; //공지사항 ID
     private String title; // 제목
     private String noticeWriter; // 작성자 이름
     private String password; // 비밀번호 (작성 시 필요)
     private String content; // 본문
-    private String writeDate; // 작성 날짜 (문자열)
+    private String regDate; // 작성 날짜 (문자열)
 }
