26fd40005533e3ad217ee86e4b2d5aea3c4ad25c
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2024-11-30 06:42:36 +0900

    의사 구현완료

---
+++ src/main/java/com/mallang/backend/dto/VacationDTO.java
@@ -0,0 +1,22 @@
+package com.mallang.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VacationDTO {
+
+    private Long id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Long doctorId;
+}
